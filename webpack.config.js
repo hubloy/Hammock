d3c135b6fadff41ad0bb27c8c549f8fe32bb4269
Paul Kevin <paultitude@gmail.com>
2021-06-02 20:55:16 +0300

    Webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,54 @@
 const path = require('path');
-const CompressionPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (env, argv) => {
-	let production = argv.mode === 'production'
+module.exports = {
 
-	return {
-		entry: {
-			'js/react/addon': path.resolve(__dirname, '_src/react/addon.js'),
-			'js/react/admin': path.resolve(__dirname, '_src/react/admin.js'),
-			'js/react/comms': path.resolve(__dirname, '_src/react/comms.js'),
-			'js/react/members': path.resolve(__dirname, '_src/react/members.js'),
-			'js/react/memberships': path.resolve(__dirname, '_src/react/memberships.js'),
-			'js/react/settings': path.resolve(__dirname, '_src/react/settings.js'),
-			'js/react/transactions': path.resolve(__dirname, '_src/react/transactions.js'),
-			'js/react/invites': path.resolve(__dirname, '_src/react/invites.js'),
-			'js/react/coupons': path.resolve(__dirname, '_src/react/coupons.js'),
-			//'js/react/shortcode': path.resolve(__dirname, '_src/react/shortcode.js'),
-			//'js/react/widget': path.resolve(__dirname, '_src/react/widget.js'),
+	entry: {
+		'js/react/addon': path.resolve(__dirname, '_src/react/addon.js'),
+		'js/react/admin': path.resolve(__dirname, '_src/react/admin.js'),
+		'js/react/comms': path.resolve(__dirname, '_src/react/comms.js'),
+		'js/react/members': path.resolve(__dirname, '_src/react/members.js'),
+		'js/react/memberships': path.resolve(__dirname, '_src/react/memberships.js'),
+		'js/react/settings': path.resolve(__dirname, '_src/react/settings.js'),
+		'js/react/transactions': path.resolve(__dirname, '_src/react/transactions.js'),
+		'js/react/invites': path.resolve(__dirname, '_src/react/invites.js'),
+		'js/react/coupons': path.resolve(__dirname, '_src/react/coupons.js')
+	},
+
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'assets'),
+	},
+
+	optimization: {
+		minimizer: [new TerserPlugin({ extractComments: false })],
+	},
+
+	resolve: {
+		alias: {
+			utils: path.resolve(__dirname, '_src/react/utils'),
+			ui: path.resolve(__dirname, '_src/react/ui'),
+			layout: path.resolve(__dirname, '_src/react/layout')
 		},
+		extensions: [".js", ".jsx", ".json"],
+	},
 
-		output: {
-			filename: '[name].js',
-			path: path.resolve(__dirname, 'assets'),
-		},
-
-		plugins: [
-			new CompressionPlugin({
-				test: /\.js(\?.*)?$/i,
-			}),
-		],
-		performance: {
-			hints: "warning",
-			// Calculates sizes of gziped bundles.
-			assetFilter: function (assetFilename) {
-				return assetFilename.endsWith(".js.gz");
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
 			},
-		},
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader,'style-loader', 'css-loader']
+			}
+		]
+	},
 
-		devtool: '',
-
-		resolve: {
-			extensions: [".js", ".jsx", ".json"],
-		},
-
-		module: {
-			rules: [
-				{
-					test: /\.jsx?$/,
-					exclude: /node_modules/,
-					loader: 'babel-loader',
-				},
-			],
-		},
-	};
+	plugins : [new MiniCssExtractPlugin()]
 }
