efaf3d1d7ad28e2dceeaed90e29e7d16e414396d
Paul Kevin <paultitude@gmail.com>
2021-06-15 22:22:53 +0300

    Stats dashboard separate

--- _src/react/admin/containers/subsite/Admin.jsx
+++ _src/react/admin/containers/subsite/Admin.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Dashboard from 'layout/Dashboard';
-import { Bar, Line } from 'react-chartjs-2';
 
 import MembersDashboard from './dashboard/members';
 import MembershipDashboard from './dashboard/memberships';
+import StatsDashboard from './dashboard/stats';
 
 export default class Admin extends Component {
 	constructor(props) {
@@ -13,69 +13,7 @@ export default class Admin extends Component {
 	}
 
 	render() {
-		const data = {
-			labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-			datasets: [
-				{
-					label: '# of Subscribers',
-					data: [12, 19, 3, 5, 2, 3, 20],
-					backgroundColor: 'rgb(49, 104, 142)',
-					borderColor: 'rgb(49, 104, 142)',
-					borderWidth: 1,
-				},
-			],
-		};
-
-		const options = {
-			scales: {
-				yAxes: [
-					{
-						ticks: {
-							beginAtZero: true,
-						},
-					},
-				],
-			},
-			plugins: {
-				legend: {
-					onClick: function(e, item) {
-						return;
-					}
-				}
-			}
-		};
-
-		const linedata = {
-			labels: ['1', '2', '3', '4', '5', '6'],
-			datasets: [
-				{
-					label: '# of Transactions',
-					data: [12, 19, 3, 5, 2, 3],
-					fill: false,
-					backgroundColor: 'rgb(49, 104, 142)',
-					borderColor: 'rgba(49, 104, 142)',
-				},
-			],
-		};
-
-		const lineoptions = {
-			scales: {
-				yAxes: [
-					{
-						ticks: {
-							beginAtZero: true,
-						},
-					},
-				],
-			},
-			plugins: {
-				legend: {
-					onClick: function(e, item) {
-						return;
-					}
-				}
-			}
-		};
+		
 		var hammock = this.props.hammock;
 		return (
 			<Dashboard hammock={hammock}>
@@ -93,16 +31,7 @@ export default class Admin extends Component {
 						</div>
 					</div>
 					<div className="uk-width-1-2@m uk-width-1-1@s">
-						<div className="uk-background-default uk-padding-small uk-panel" uk-height-viewport="min-height: 800">
-							<p className="uk-h4">{hammock.strings.stats.title}</p>
-							<div>
-								<Bar data={data} options={options}/>
-							</div>
-							<hr/>
-							<div>
-								<Line data={linedata} options={lineoptions} />
-							</div>
-						</div>
+						<StatsDashboard hammock={hammock}/>
 					</div>
 				</div>
 			</Dashboard>
---
+++ _src/react/admin/containers/subsite/dashboard/stats.jsx
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+
+import fetchWP from 'utils/fetchWP';
+
+import {Preloader, Center} from 'ui/admin/form';
+import { Bar, Line } from 'react-chartjs-2';
+
+export default class StatsDashboard extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading_subs : true,
+			loading_trans : true,
+			subscribers : [],
+			transactions : []
+        };
+        this.fetchWP = new fetchWP({
+			api_url: this.props.hammock.api_url,
+			api_nonce: this.props.hammock.api_nonce,
+        });
+	}
+
+
+	componentDidMount() {
+		Promise.all([this.load_subscribers(), this.load_transactions()]);
+	}
+
+	load_subscribers = async() => {
+		this.fetchWP.get( 'dashboard/subscribers' )
+			.then( (json) => this.setState({
+				subscribers : json,
+				loading_subs : false,
+				error : false,
+			}), (err) => this.setState({ loading_subs : false, error : true })
+		);
+	}
+
+	load_transactions = async() => {
+		this.fetchWP.get( 'dashboard/transactions' )
+			.then( (json) => this.setState({
+				transactions : json,
+				loading_trans : false,
+				error : false,
+			}), (err) => this.setState({ loading_trans : false, error : true })
+		);
+	}
+
+
+	render() {
+		var subscribers = this.state.subscribers;
+		var transactions = this.state.transactions;
+		var hammock = this.props.hammock;
+		const data = {
+			labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+			datasets: [
+				{
+					label: '# of Subscribers',
+					data: [12, 19, 3, 5, 2, 3, 20],
+					backgroundColor: 'rgb(49, 104, 142)',
+					borderColor: 'rgb(49, 104, 142)',
+					borderWidth: 1,
+				},
+			],
+		};
+
+		const options = {
+			scales: {
+				yAxes: [
+					{
+						ticks: {
+							beginAtZero: true,
+						},
+					},
+				],
+			},
+			plugins: {
+				legend: {
+					onClick: function(e, item) {
+						return;
+					}
+				}
+			}
+		};
+
+		const linedata = {
+			labels: ['1', '2', '3', '4', '5', '6'],
+			datasets: [
+				{
+					label: '# of Transactions',
+					data: [12, 19, 3, 5, 2, 3],
+					fill: false,
+					backgroundColor: 'rgb(49, 104, 142)',
+					borderColor: 'rgba(49, 104, 142)',
+				},
+			],
+		};
+
+		const lineoptions = {
+			scales: {
+				yAxes: [
+					{
+						ticks: {
+							beginAtZero: true,
+						},
+					},
+				],
+			},
+			plugins: {
+				legend: {
+					onClick: function(e, item) {
+						return;
+					}
+				}
+			}
+		};
+		return (
+			<div className="uk-background-default uk-padding-small uk-panel" uk-height-viewport="min-height: 800">
+				<p className="uk-h4">{hammock.strings.stats.title}</p>
+				<div>
+					<Bar data={data} options={options}/>
+				</div>
+				<hr/>
+				<div>
+					<Line data={linedata} options={lineoptions} />
+				</div>
+			</div>
+		)
+	}
+}
