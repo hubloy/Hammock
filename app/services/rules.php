ec4c20b7b5350008804f2da538a21f32462b2d60
Paul Kevin <paultitude@gmail.com>
2021-11-04 20:25:06 +0300

    Build

--- app/services/rules.php
+++ app/services/rules.php
@@ -15,4 +15,33 @@ use Hammock\Model\Rule;
  */
 class Rules {
 
+	/**
+	 * The table name
+	 *
+	 * @var string
+	 */
+	private $table_name;
+
+	/**
+	 * Main service constructor
+	 *
+	 * Sets up the service
+	 */
+	public function __construct() {
+		$this->table_name = Database::get_table_name( Database::MEMBERSHIP_RULES );
+	}
+
+	/**
+	 * Get rules by type
+	 * 
+	 * @param string $type - the object type
+	 * @param int $id - the object id
+	 * 
+	 * @since 1.0.0
+	 * 
+	 * @return array
+	 */
+	public function get_rules( $type, $id ) {
+
+	}
 }
