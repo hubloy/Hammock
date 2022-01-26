5080bd5caa9e7e8f6c057a43f10246213d77a025
Paul Kevin <paultitude@gmail.com>
2022-01-26 22:25:49 +0300

    Fix pagination

--- _src/react/admin/ui/admin/elements/Pagination.jsx
+++ _src/react/admin/ui/admin/elements/Pagination.jsx
@@ -22,25 +22,25 @@ export class PaginationUI extends PureComponent {
 				{pager.pages && pager.pages.length > 0 &&
 					<ul className="uk-pagination uk-flex-center" uk-margin="">
 						<li className={`${pager.current === 1 ? 'uk-disabled' : ''}`}>
-							<Link to={{  pathname: `${base}/page/1` }} data-page={1} onClick={this.onClick}><span uk-icon="chevron-double-left"></span></Link>
+							<Link to={{  pathname: `/${base}/page/1` }} data-page={1} onClick={this.onClick}><span uk-icon="chevron-double-left"></span></Link>
 						</li>
 						<li className={`${pager.current === 1 ? 'uk-disabled' : ''}`}>
-							<Link to={{ pathname: `${base}/page/${pager.current - 1}` }} data-page={pager.current - 1} onClick={this.onClick}><span uk-icon="chevron-left"></span></Link>
+							<Link to={{ pathname: `/${base}/page/${pager.current - 1}` }} data-page={pager.current - 1} onClick={this.onClick}><span uk-icon="chevron-left"></span></Link>
 						</li>
 						{pager.pages.map(page =>
 							<li key={page} className={`${pager.current === page ? 'uk-active' : ''}`}>
 								{parseInt(Number(page)) > 0 ? (
-									<Link to={{pathname : `${base}/page/${page}` }} data-page={page} onClick={this.onClick}>{page}</Link>
+									<Link to={{pathname : `/${base}/page/${page}` }} data-page={page} onClick={this.onClick}>{page}</Link>
 								): (
 									<span>{page}</span>
 								)}
 							</li>
 						)}
 						<li className={`${pager.current === pager.total ? 'uk-disabled' : ''}`}>
-							<Link to={{ pathname: `${base}/page/${pager.current + 1}` }} data-page={pager.current + 1} onClick={this.onClick}><span uk-icon="chevron-right"></span></Link>
+							<Link to={{ pathname: `/${base}/page/${pager.current + 1}` }} data-page={pager.current + 1} onClick={this.onClick}><span uk-icon="chevron-right"></span></Link>
 						</li>
 						<li className={`${pager.current === pager.total ? 'uk-disabled' : ''}`}>
-							<Link to={{ pathname: `${base}/page/${pager.total}` }} data-page={pager.total} onClick={this.onClick}><span uk-icon="chevron-double-right"></span></Link>
+							<Link to={{ pathname: `/${base}/page/${pager.total}` }} data-page={pager.total} onClick={this.onClick}><span uk-icon="chevron-double-right"></span></Link>
 						</li>
 					</ul>
 				}
